ead6bab56a1730c2f54f7939bccb22850cf486ef
bpahunt <bpahunt@googlemail.com>
2024-05-17 13:22:21 +0100

    Update getComputerChoice to return rock, paper, scissors

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let computerChoice
-
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0) {
+        return "Rock";
+    }   else if (computerChoice == 1) {
+        return "Paper"
+    }   else {
+        return "Scissors"
+    }
 }
 
 console.log(getComputerChoice())
