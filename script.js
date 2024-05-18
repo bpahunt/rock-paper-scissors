4911f6a23acdb85c92d7daedde189bd03e870de4
bpahunt <bpahunt@googlemail.com>
2024-05-18 19:59:06 +0100

    Add check for null input, add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to generate random choice for the computer of "Rock", "Paper" or "Scissors"
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
@@ -9,19 +10,27 @@ function getComputerChoice() {
     }
 }
 
+// Function to prompt user for input of "Rock", "Paper" or "Scissors"
 function getHumanChoice() {
     let humanChoice
+    // Loop until valid input is received
     while (true) {
-        let humanChoice = prompt("Rock, Paper or Scissors?")
+        humanChoice = prompt("Rock, Paper or Scissors?")
+        // Check for null input (e.g. if cancel button is pressed)
+        if (humanChoice == null) {
+            continue
+        }
+        // Check if input matches an expected value, ignores case sensitivity
         if (humanChoice.toUpperCase() == "ROCK") {
             return "Rock"
         }   else if (humanChoice.toUpperCase() == "PAPER") {
             return "Paper" 
         }   else if (humanChoice.toUpperCase() == "SCISSORS") {
             return "Scissors"
-        }
+        }   
     }
 }
 
+// Log player and computer choices to console
 console.log("Player chooses " + getHumanChoice())
 console.log("Computer chooses " + getComputerChoice())
